b290d4c94d05b86e43c5ed0be24da169ca005dc7
sgao <sgao@redhat.com>
2020-09-03 13:30:13 +0800

    change web content

--- examples/setup-test-app/testapp.py
+++ examples/setup-test-app/testapp.py
@@ -1,4 +1,4 @@
 
 def application(environ, start_response):
     start_response('200 OK', [('Content-Type','text/plain')])
-    return [b"Hello from gunicorn WSGI application!"]
+    return [b"Linux Container Web Server"]
